d417afb7d2f364855aca2c1f417b6c8a6de94f4a
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 20:12:13 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,10 +2,12 @@ class GamesController < ApplicationController
   # Add your GamesController code here
 
   def index
+    
   end
 
   def create
     @game = Game.new(game_params)
+    redirect_to game_path(@game)
   end
 
   def show
