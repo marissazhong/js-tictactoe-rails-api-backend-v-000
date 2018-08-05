46e0ea9f6e9781a619a77a220835c01db03eece1
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 20:24:22 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,31 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
 
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def create
+    @game = Game.create(state: params[:state])
+    render json: @game, status: 201
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+
+  def update
+    game = Game.find(params[:id])
+    game.state = params[:state]
+    game.save
+    render json: game
+  end
+
+  # private
+  #
+  # def game_params
+  #   params.require(:game).permit(:state)
+  # end
 end
